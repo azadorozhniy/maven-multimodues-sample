5f16194065bf4d1b1180a4f119566a8ec53c0482
Artem <azadorozhniy@lohika.com>
2013-12-24 16:19:42 +0200

    Added multy module project

---
+++ module1/src/main/java/info/mvnmoduels/module1/Moudle1.java
@@ -0,0 +1,12 @@
+package info.mvnmoduels.module1;
+
+/**
+ * Created by artemzadorozhniy on 12/24/13.
+ */
+public class Moudle1 {
+
+    public void module1Mehtod() {
+        System.out.print("hello module 1");
+    }
+
+}
---
+++ module1/submodule1/src/main/java/info/mvnmoduels/module1/submodule1/Submodule1.java
@@ -0,0 +1,9 @@
+package info.mvnmoduels.module1.submodule1;
+
+public class Submodule1 {
+
+    public void subModule1Mehtod() {
+        System.out.print("hello sub module 2");
+    }
+
+}
